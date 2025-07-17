7959348f4137528a4e6b1ead7facef90fc9a1efc
Abindavis508 <abindavis18@gmail.com>
2025-07-17 13:23:32 +0530

    files modified

---
+++ switchCase_condition.js
@@ -0,0 +1,18 @@
+let username = "abindavis";
+
+switch (username) {
+    case "abindavis":
+        console.log("Abin is an active user");
+        break;
+
+    case "sharon":
+        console.log("inactive user");
+        break;
+
+    case "fathaah":
+        console.log("inactive user");
+        break;
+    default:
+        console.log("user not found");
+        break;
+}
