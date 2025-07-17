9a2a49e6e10c891561f25f31c8770c0eee2e41d2
Abindavis508 <abindavis18@gmail.com>
2025-07-17 12:55:18 +0530

    file modified

---
+++ if _else_if _ladder_condition.js
@@ -0,0 +1,12 @@
+let speed=50;
+if(speed==90){
+    console.log("you are over the speed limit");
+}
+else if(speed<=60){
+    console.log("you are within the speed limit");
+}
+else if(speed==100){
+    console.log("life at risk go slow");
+}else{
+    console.log("speed cannot detected");
+}
