796ccacfef41fcf2007264060cdaec16f82a0582
Abindavis508 <abindavis18@gmail.com>
2025-07-17 13:32:22 +0530

    files modified

---
+++ NestedIf_example.js
@@ -0,0 +1,17 @@
+let no1=20
+let no2=30
+let no3=40
+
+if (no1>no2) {
+    if (no1>no3) {
+        console.log("no1 is the largest number");
+    } else {
+        console.log("no3 is the largest number");
+    }
+}else {
+    if (no2>no3) {
+        console.log("no2 is the largest number");
+    } else {
+        console.log("no3 is the largest number");
+    }
+}
