9d77449f57cd330be98a01997f518cddfcec7bb3
Abindavis508 <abindavis18@gmail.com>
2025-07-17 13:14:21 +0530

    new files added

---
+++ NestedIF.js
@@ -0,0 +1,13 @@
+let username="alpha";
+let password="1234";
+
+if(username==="alpha"){
+    
+    if (password==="1234"){
+        console.log("login succesful");
+    }else{
+        console.log("password is incorrect");
+    }
+}else {
+    console.log("username is incorrect");
+}
